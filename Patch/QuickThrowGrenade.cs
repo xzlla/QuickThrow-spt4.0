55cc54b6d4c37dd324c43b759f9bd4f0e6ad55ad
xzlla <105400817+xzlla@users.noreply.github.com>
2026-03-12 10:20:44 +0800

    Add files via upload

---
+++ Patch/QuickThrowGrenade.cs
@@ -0,0 +1,62 @@
+﻿using Comfort.Common;
+using HarmonyLib;
+using EFT;
+using QuickThrow.Utils;
+using UnityEngine;
+
+namespace QuickThrow.Patch
+{
+    [HarmonyPatch(typeof(Player))]
+    internal class QuickThrowGrenade
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("Proceed", typeof(ThrowWeapItemClass), typeof(Callback<IHandsThrowController>), typeof(bool))]
+        public static bool Prefix(Player __instance, ThrowWeapItemClass throwWeap, Callback<IHandsThrowController> callback)
+        {
+            if (Plugin.DisableFastGrenade.Value)
+                return true;
+            if (!__instance.IsYourPlayer)
+                return true;
+            if (Input.GetKey(Plugin.KeyboardBindingOrigine.Value.MainKey))
+                return true;
+            
+            __instance.Proceed(throwWeap,
+                new Callback<GInterface206>(result =>
+                {
+                    QuickThrowLogger.Log($"[QuickThrowGrenade] Proceed (QuickUse) result = {result}");
+                }), false);
+            return false;
+        }
+        
+        [HarmonyPatch(typeof(Player.BaseGrenadeHandsController), "vmethod_2")]
+        public class ForceLowThrow
+        {
+            [HarmonyPrefix]
+            public static void Prefix(
+                ref float timeSinceSafetyLevelRemoved,
+                ref bool lowThrow,
+                Player.BaseGrenadeHandsController __instance)
+            {
+                if (Plugin.DisableFastGrenade.Value)
+                    return;
+                    
+                var playerField = AccessTools.Field(typeof(Player.BaseGrenadeHandsController), "_player");
+                if (playerField?.GetValue(__instance) is not Player player)
+                    return;
+
+                if (__instance is Player.QuickGrenadeThrowHandsController && player.IsYourPlayer)
+                {
+                    if (!Input.GetKey(Plugin.KeyboardBindingShort.Value.MainKey))
+                        return;
+
+                    lowThrow = true;
+                    QuickThrowLogger.Log($"[QuickThrowGrenade] Forced low throw (QuickThrow) | timeSinceSafetyLevelRemoved={timeSinceSafetyLevelRemoved}, lowThrow={lowThrow}");
+                }
+                else
+                {
+                    QuickThrowLogger.Log($"[QuickThrowGrenade] Normal throw | timeSinceSafetyLevelRemoved={timeSinceSafetyLevelRemoved}, lowThrow={lowThrow}");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+﻿# 🎯 QuickThrowGrenade
+
+This mod for **SPT** restores the deprecated "Fast Grenade Throw" mechanic from earlier Tarkov versions (≈ <0.12), allowing you to **instantly throw a grenade** without having to equip it first.
+
+PS：The original author didn't update the SPT 4.0 version, and I didn't see a usable QuickThrow in SPT 4.0 either, so I ported it over.
+
+The original author's website: https://github.com/yox92/QuickThrow
+
+## 🔧 Key Features
+
+- **Fast throw grenade**: Pressing the grenade key will throw the grenade instantly, skipping equip animations.
+- **Low throw override**: Hold a configurable 'Left Control' key to **force a short throw** with QuickThrow feature.
+- **Restore Vanilla**: Hold a configurable key 'Shift' to **to equip it first**. Vanilla feature.
+
+## 🔌 Installation
+
+Drop `QuickThrow.dll`, `QuickThrow_log.txt`, `debug.cfg` folder into your `BepInEx/plugins/QuickThrow` directory.
+
+## 🧠 How It Works
+
+This mod uses Harmony patches on two key methods:
+- `Player.Proceed(ThrowWeapItemClass, Callback<IHandsThrowController>, bool)`  
+  → Replaced with `Proceed(ThrowWeapItemClass, Callback<GInterface206>, bool)` (QuickUse) unless a bypass key is held.
+- `Player.BaseGrenadeHandsController.vmethod_2(float timeSinceSafetyLevelRemoved, Vector3 position, Quaternion rotation, Vector3 force, bool lowThrow)`  
+  → Dynamically enforces **low throw** if the appropriate key is pressed.
+
+## 🖱️ Keybindings
+
+| Action              | Default Key                            |
+|---------------------|----------------------------------------|
+| Force low throw     | `Left Control` or any configurable key |
+| Force Vanilla throw | `Left Shift` or any configurable key   |
+
+## ⚙️ Debug
+A detailed log is printed to `QuickThrow_log.txt` when file `debug.cfg` true.
